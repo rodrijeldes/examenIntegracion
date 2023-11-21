993770ac36981cd5189b691feed445a9b6d6501d
Rodrigo Jeldes Cisternas <rodrigo.jeldes@ipchile.cl>
2023-11-21 16:55:05 -0300

    modificacion3

---
+++ src/main/java/DB/conexion.java
@@ -0,0 +1,19 @@
+package DB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class conexion {
+
+Connection con;
+	
+	public Connection getConnection() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/petsmile?serverTimezone=UTC","root","root");
+		}
+		catch (Exception ex){}
+		return con;
+	}
+}
+
